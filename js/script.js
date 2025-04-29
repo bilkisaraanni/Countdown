2ee39cebe56b408a388f9848fc91908924a67906
bilkisaraanni <bilkisara28@mail.com>
2025-04-30 00:41:58 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+  let targetDate = new Date("April 30, 2025 00:00:00").getTime();
+
+    function timer(){
+     let todayDate = new Date().getTime();
+     let distance = targetDate - todayDate;
+
+     let days = Math.floor(distance/1000/60/60/24); 
+     let hours = Math.floor(distance/1000/60/60) %24;      
+     let minutes = Math.floor(distance/1000/60) %60; 
+     let seconds = Math.floor(distance/1000)%60; 
+
+     document.getElementById("countdown").innerHTML = (`Days${days}:Hours${hours}:Minites${minutes}:Seconds${seconds}`);
+
+   }
+   timer()
+
+   setInterval(timer,1000);
+
+  
+
+// let num = 20.4566;
+
+// let number = parseFloat(num)
+
+// console.log(num.toFixed(3));
